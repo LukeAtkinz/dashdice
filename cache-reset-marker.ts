ddd45265240016b8ed83084f0ac6831933571df2
LukeAtkinz <david.lukeatkins@gmail.com>
2025-10-07 12:36:43 +0100

    Force redeploy to reset proxy cache - backend connection fix

---
+++ cache-reset-marker.ts
@@ -0,0 +1,4 @@
+// Cache reset marker
+// Updated at: 2025-10-07 11:36 UTC
+// This file forces a new deployment to reset proxy cache
+export const CACHE_RESET_TIMESTAMP = new Date('2025-10-07T11:36:00Z').getTime();
